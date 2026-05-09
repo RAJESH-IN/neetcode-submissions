997beba6b9bd7b9f3ef50cf7afc9196a9e623f07
Rajesh Kumar Mahto <57666810+RAJESH-IN@users.noreply.github.com>
2026-05-09 12:29:40 +0530

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.cs
@@ -0,0 +1,51 @@
+public class LRUCache
+{
+    private int capacity;
+
+    private Dictionary<int, int> cache;
+
+    private List<int> order;
+
+    public LRUCache(int capacity)
+    {
+        this.capacity = capacity;
+
+        cache = new Dictionary<int, int>();
+
+        order = new List<int>();
+    }
+
+    public int Get(int key)
+    {
+        if(!cache.ContainsKey(key))
+            return -1;
+
+        order.Remove(key);
+        order.Add(key);
+        return cache[key];    
+       
+    }
+
+    public void Put(int key, int value)
+    {
+         if(cache.ContainsKey(key))
+        {
+            cache[key] = value;
+
+            order.Remove(key);
+            order.Add(key);
+        }
+        else
+        {
+            if(cache.Count == capacity)
+            {
+                int lru = order[0];
+                order.RemoveAt(0);
+                cache.Remove(lru);
+            }
+            cache[key] = value;
+            order.Add(key);
+        }
+       
+    }
+}
