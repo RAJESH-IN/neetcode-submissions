0d73822336edd50d455185a90d7d93e0c0a2af6c
Rajesh Kumar Mahto <57666810+RAJESH-IN@users.noreply.github.com>
2026-05-14 05:34:11 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int NumRescueBoats(int[] people, int limit) {
+        Array.Sort(people);
+        int left=0;
+        int right=people.Length-1;
+        int boat=0;
+
+        while(left<=right)
+        {
+            if(people[left]+people[right]<=limit)
+            {
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+}
